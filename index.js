96abfa4fe2fc95914195bb0dd22a8a933cad0db8
simokh <m.simokhouili@gmail.com>
2021-03-01 23:50:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, surprise) {
+    let new_array = []
+    for (let i = 0; i < name.length; i++) {
+    new_array.push(`Thank you, ${name[i]}, for the wonderful ${surprise} gift!`)
+    }
+    return new_array;
+}
+
+
+function countDown(num) {
+
+    while (num >= 0) {
+        console.log(num--);
+}
+}
+
+
